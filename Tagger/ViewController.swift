725c7bcac657c90bea729b28dea719b7452d1520
Ivan Magda <imagda15@gmail.com>
2016-06-03 13:09:31 +0900

    Initial Commit

---
+++ Tagger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tagger
+//
+//  Created by Ivan Magda on 03/06/16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
