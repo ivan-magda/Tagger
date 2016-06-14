3c0e00d0a017f5817c1e10d75669006a5ae5ceab
Ivan Magda <imagda15@gmail.com>
2016-06-14 14:33:32 +0900

    Polish up the ImageTaggerViewController UI

--- Tagger/ImageTaggerViewController.swift
+++ Tagger/ImageTaggerViewController.swift
@@ -22,7 +22,15 @@
 
 import UIKit
 
-// MARK: ImageTaggerViewController: UIViewController, Alertable
+// MARK: Types
+
+private enum UIState {
+    case Default
+    case Networking
+    case DoneWithNetworking
+}
+
+// MARK: - ImageTaggerViewController: UIViewController, Alertable
 
 class ImageTaggerViewController: UIViewController, Alertable {
     
@@ -37,7 +45,9 @@ class ImageTaggerViewController: UIViewController, Alertable {
     
     @IBOutlet weak var cancelBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var processOnImageButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var generateBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var resultsBarButtonItem: UIBarButtonItem!
     
     // MARK: - View Life Cycle
     
@@ -53,25 +63,23 @@ class ImageTaggerViewController: UIViewController, Alertable {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
-    @IBAction func processOnImageButtonDidPressed(sender: AnyObject) {
-        if generatedTags != nil {
-            showTags()
-        } else {
-            imaggaApiClient.taggingImage(taggingImage, successBlock: { [unowned self] tags in
-                self.generatedTags = tags
-                self.showTags()
-            }) { [unowned self] error in
-                let alertController = self.alert("Error", message: error.localizedDescription, handler: nil)
-                self.presentViewController(alertController, animated: true, completion: nil)
-            }
+    @IBAction func generateTags(sender: AnyObject) {
+        setUIState(.Networking)
+        imaggaApiClient.taggingImage(taggingImage, successBlock: { [unowned self] tags in
+            self.generatedTags = tags
+            self.setUIState(.DoneWithNetworking)
+            self.showResults(self)
+        }) { [unowned self] error in
+            self.generatedTags = nil
+            self.setUIState(.DoneWithNetworking)
+            let alertController = self.alert("Error", message: error.localizedDescription, handler: nil)
+            self.presentViewController(alertController, animated: true, completion: nil)
         }
     }
     
-    // MARK: - Helpers
-    
-    private func showTags() {
+    @IBAction func showResults(sender: AnyObject) {
         let tagListViewController = TagListViewController()
-        tagListViewController.title = "Generated Tags"
+        tagListViewController.title = "Results"
         tagListViewController.tags = generatedTags!
         navigationController?.pushViewController(tagListViewController, animated: true)
     }
@@ -83,8 +91,31 @@ class ImageTaggerViewController: UIViewController, Alertable {
 extension ImageTaggerViewController {
     
     private func configureUI() {
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
-        imageView.image = taggingImage
+        setUIState(.Default)
+        navigationController?.view.backgroundColor = .whiteColor()
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .Plain, target: nil, action: nil)
+    }
+    
+    private func setUIState(state: UIState) {
+        func updateResultsButtonState() {
+            resultsBarButtonItem.enabled = generatedTags != nil && generatedTags?.count > 0
+        }
+        
+        switch state {
+        case .Default:
+            imageView.image = taggingImage
+            resultsBarButtonItem.enabled = false
+        case .Networking:
+            UIUtils.showNetworkActivityIndicator()
+            activityIndicator.startAnimating()
+            resultsBarButtonItem.enabled = false
+            generateBarButtonItem.enabled = false
+        case .DoneWithNetworking:
+            UIUtils.hideNetworkActivityIndicator()
+            activityIndicator.stopAnimating()
+            generateBarButtonItem.enabled = true
+            updateResultsButtonState()
+        }
     }
     
 }
