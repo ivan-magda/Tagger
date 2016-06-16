63580bf779b6b6820a3d3188d9b04197cabff3d4
Ivan Magda <imagda15@gmail.com>
2016-06-17 01:38:52 +0900

    Implement Flickr OAuth user authentication process

--- Tagger/AppDelegate.swift
+++ Tagger/AppDelegate.swift
@@ -34,10 +34,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UIApplicationDelegate
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
         if let tabBarController = window?.rootViewController as? UITabBarController {
             tabBarController.selectedIndex = 1
         }
+        
         return true
     }
 
